7db0eb9a63a89b99c351e9f0042384f2c97315ad
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-05-09 21:52:38 -0400

    added question array

---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+const questions = [
+    {
+        title: "Question 1: A property of an object that is a function is called a ____.",
+        choices: ["string", "stylesheet", "method", "boolean"],
+        answer: "method"
+    },
+    {
+        title: "Question 2: Commonly used data types do NOT include:",
+        choices: ["alerts", "booleans", "numbers", "strings"],
+        answer: "alerts"
+    },
+    {
+        title: "Question 3: The logical operator that represents 'or' is:",
+        choices: ["||", "OR", "&&", "==="],
+        answer: "||"
+    },
+    {
+        title: "Question 4: The condition in an if / else statement is enclosed within:",
+        choices: ["quotes", "parentheses", "square brackets", "curly brackets"],
+        answer: "parentheses"
+    },
+    {
+
+        title: "Question 5: The instructions for a function are enclosed within:",
+        choices: ["quotes", "parentheses", "square brackets", "curly brackets"],
+        answer: "curly brackets"
+    },
+];
